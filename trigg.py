b05a0ae740ade3bdb2955b6307130bba579d5cfb
Vince <knight.fo@yandex.ru>
2022-02-16 23:35:55 +0300

    alpha works cos sin and tg

--- trigg.py
+++ trigg.py
@@ -1,3 +1,4 @@
+
 from math import *
 
 
@@ -10,14 +11,42 @@ def costosin(n):
 	sinus = sqrt(1-n**2)
 	return(sinus)
 
+def tantocosandsin(n):
+	cossin = (sqrt(1/(1+n**2)))
+	return(cossin)
 
-choose = input(''' 
+
+while True:
+  
+	choose = str(input(''' 
 [1] - Синус
 [2] - Косинус
-: ''')
-x = float(input())
-if choose == 1:
-	print(sintocos(x))
-else:
-	print(costosin(x))	
+[3] - Тангенс
+[exit] - Выход
+: '''))
+	
 
+
+	if choose == '1':
+			x = float(input())
+			if sintocos(x) == 0:
+				print('| Sin: ',x,' | ')
+				print('| Cos: ',sintocos(x), '|')
+			else:
+				print('| Sin: ',x,' | ')
+				print('| Cos: ',sintocos(x), '|')
+				print('| Tg: ',x/sintocos(x), '|')
+
+	elif choose == '2' :
+		x = float(input())
+		print('| Cos: ', x, '|')
+		print('| Sin: ', costosin(x), '|')
+		print('| Tg: ', costosin(x)/(x),'|')
+	elif choose == '3':
+		x = float(input())
+		print('| Tg: ',x,'|')
+		print('| Cos:', tantocosandsin(x), '|')	
+	elif choose == 'exit':
+		break
+	else:
+		print("Ошибка выбора")
