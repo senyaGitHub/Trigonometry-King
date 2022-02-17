c27495de4db07f90ecbec0317c5d9801c57235ae
Vince <knight.fo@yandex.ru>
2022-02-17 20:22:47 +0300

    colorama added, bug fixed

--- trigg.py
+++ trigg.py
@@ -1,70 +1,56 @@
-
 from math import *
+from colorama import *
+import os
+os.system('clear') 
 
-
-def sintocos(n):
+def Co(n):
 	cosin = sqrt(1-n**2)
 	return(cosin)
 
 
-def costosin(n):
+def Si(n):
 	sinus = sqrt(1-n**2)
 	return(sinus)
 
 
-def tantocos(n):
+def Tg(n):
 	cossin = (sqrt(1/(1+n**2)))
 	return(cossin) 
 
 
-while True: 
+
   
-	choose = str(input('''
+choose = str(input('''
 [1] - Синус
 [2] - Косинус
 [3] - Тангенс
-[exit] - Выход
 : '''))# 'Эта часть кода отвечает за вопрос и вывод информатии'
 	
-
-
-	if choose == '1':
-			x = float(input())
-			if sintocos(x) == 0:
-				print('| Sin: ',x,' | ')
-				print('| Cos: ',sintocos(x), '|')
-			else:
-				print('| Sin: ',x,' | ')
-				print('| Cos: ',sintocos(x), '|')
-				print('| Tg: ',x/sintocos(x), '|')
-
-	elif choose == '2' :
-		x = float(input())
-		print('| Sin: ', costosin(x), '|')
-		print('| Cos: ', x, '|')
-		print('| Tg: ', costosin(x)/(x),'|')
-	elif choose == '3':
-		x = float(input())
-		print('| Sin: ',sqrt(1-(tantocos(x)**2)))
-		print('| Cos:', tantocos(x), '|')	
-		print('| Tg: ',x,'|')
-	elif choose == 'exit':
-		break
+if choose == '1':
+	x = float(input('x = ?: '))
+	if Co(x) == 0:
+		print(Fore.RED + '| Sin: ',x,' | ')
+		print(Fore.GREEN + '| Cos: ',Co(x), '|')
 	else:
-		print("Ошибка выбора")
+		print(Fore.RED+'| Sin: ',x,' | ')
+		print(Fore.GREEN+'| Cos: ',Co(x), '|')
+		print(Fore.BLUE+'| Tg: ',x/Co(x), '|')
+
+elif choose == '2' :
+	x = float(input('x = ?: '))
+	print(Fore.RED+'| Sin: ', Si(x), '|')
+	print(Fore.GREEN+'| Cos: ', x, '|')
+	print(Fore.BLUE+'| Tg: ', Si(x)/(x),'|')
+elif choose == '3':
+	x = float(input('x = ?: '))
+	print(Fore.RED+'| Sin: ',Si(Tg(x)), '|')
+	print(Fore.GREEN+'| Cos:', Tg(x), '|')	
+	print(Fore.BLUE+'| Tg: ',x,'|')
+else:
+	print("Ошибка выбора")
 
 
 
-
-
-ch = input(''' 
-В каком формате данные?
-[1]
-[2]
-[3] ''')
-
-if  ch == '1': # для выбора из градусов радианов и значений
-	pass
 
 
 
